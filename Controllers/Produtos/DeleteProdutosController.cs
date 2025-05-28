8c8625f6186cf0d16659fc4f3de6086b61640448
mateus.maia <pmaiamateus@gmail.com>
2025-05-28 17:04:24 -0300

    cria controller para delete produtos

---
+++ Controllers/Produtos/DeleteProdutosController.cs
@@ -0,0 +1,31 @@
+﻿using BlueProjectTest.Data;
+using BlueProjectTest.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueProjectTest.Controllers.Produtos;
+
+public class DeleteProdutosController : Controller
+{
+    [AllowAnonymous]
+    [HttpDelete("produtos/{id:int}")]
+    public async Task<IActionResult> DeleteAsync(
+		[FromRoute] int id,
+		[FromServices] AppDbContext context)
+    {
+		try
+		{
+            Produto? procuraProduto = await context.Produtos.FirstOrDefaultAsync(u => u.ProdutoId == id);
+			if (procuraProduto == null)
+				return NotFound();
+			context.Remove(procuraProduto);
+			context.SaveChanges();
+            return Ok();
+		}
+		catch (Exception)
+		{
+			throw;
+		}
+    }
+}
