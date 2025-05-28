5f4bdf8a62948b29b48a6ac4c1b06dbd3e80294c
mateus.maia <pmaiamateus@gmail.com>
2025-05-28 17:54:44 -0300

    adiciona validação de propriedades de produtos

--- Models/ProdutoModel.cs
+++ Models/ProdutoModel.cs
@@ -3,9 +3,9 @@
 public class Produto
 {
     public int ProdutoId { get; set; } = 0;
-    public string Nome { get; set; } = string.Empty;
-    public string Descricao { get; set; } = string.Empty;
-    public decimal Preco { get; set; } = decimal.Zero;
-    public int Estoque { get; set; } = 0;
+    public string Nome { get; set { if (value.Length > 100) throw new ArgumentException("Tamanho máximo de 50 caracteres"); else Nome = value; } } = string.Empty;
+    public string Descricao { get; set { if (value.Length > 255) throw new ArgumentException("Tamanho máximo de 255 caracteres"); else Descricao = value; } } = string.Empty;
+    public decimal Preco { get; set { if (value < 0) throw new ArgumentException("Valor não pode ser menor que zero"); else Preco = value; } } = decimal.Zero;
+    public int Estoque { get; set { if (value < 0) throw new ArgumentException("Valor não pode ser menor que zero"); else Estoque = value; } } = 0;
     public DateTime DataCadastro { get; set; }
 }
